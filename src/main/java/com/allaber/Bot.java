ca7ad477ee1535e26ab75a4ead8e0e314d8746a9
ALLABER <ubermenschalone@gmail.com>
2022-06-02 23:25:04 +0500

    Простой echo бот

---
+++ src/main/java/com/allaber/Bot.java
@@ -0,0 +1,38 @@
+package com.allaber;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.name}")
+    private String botUsername;
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            String chatId = String.valueOf(update.getMessage().getChatId());
+            execute(new SendMessage(chatId, "Привет, " + update.getMessage().getChat().getFirstName()+ "!"));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/allaber/bot/Application.java
+++ src/main/java/com/allaber/bot/Application.java
@@ -1,4 +1,4 @@
-package com.allaber;
+package com.allaber.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
